7b9a425ebb29801f79ee34dd892bdc78966376a6
Cheuk Yin Ng <cheukyin699@yahoo.com>
2016-05-16 13:41:45 -0700

    Added the skeleton for the states

---
+++ src/menu.cc
@@ -0,0 +1,14 @@
+#include "menu.h"
+
+MenuState::MenuState() {
+}
+
+void MenuState::handle() {
+}
+
+void MenuState::update(Time) {
+}
+
+void MenuState::draw(RenderWindow&) {
+}
+
---
+++ src/play.cc
@@ -0,0 +1,14 @@
+#include "play.h"
+
+PlayState::PlayState() {
+}
+
+void PlayState::handle() {
+}
+
+void PlayState::update(Time) {
+}
+
+void PlayState::draw(RenderWindow&) {
+}
+
