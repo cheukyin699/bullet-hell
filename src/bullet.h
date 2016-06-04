44a0aba1d2f70f48c9f50a18880c77d9300c8c56
Cheuk Yin Ng <cheukyin699@yahoo.com>
2016-06-03 19:21:49 -0700

    Add bullet skeleton

---
+++ src/bullet.h
@@ -0,0 +1,16 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Bullet : public Sprite {
+    private:
+
+    public:
+        Bullet();
+};
+
+#endif
+
